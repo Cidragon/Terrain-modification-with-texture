0831e3a0d78ecb3ec384948de8fcbb108050381a
Cidragon <darioperezpastrian@gmail.com>
2023-12-25 13:26:42 -0300

    misspelling errors

--- Terrain manager.gd
+++ Terrain manager.gd
@@ -1,27 +1,27 @@
 extends Node2D
 
-@export var heigth_texture : Texture2D
+@export var height_texture : Texture2D
 @export var tile_texture : Texture2D
 
-var heigth_sprites : Array[Sprite2D] = []
+var height_sprites : Array[Sprite2D] = []
 
 func _ready() -> void:
 	set_terrain_width_2(Main.TERRAIN_MAX_WIDTH)
 
 func update_terrain(index : int, value : int) -> void:
-	heigth_sprites[index].scale.y = value
-	heigth_sprites[index].position.y = 636 - (2 * (value-1))
+	height_sprites[index].scale.y = value
+	height_sprites[index].position.y = 636 - (4 * (value-1))
 
 func set_terrain_width(width : int) -> void:
 	for i in range(width):
 		var new_sprite : Sprite2D =  Sprite2D.new()
-		new_sprite.texture = heigth_texture
+		new_sprite.texture = height_texture
 		new_sprite.centered = false
-		new_sprite.position = Vector2(i * Main.TEXTURE_SIZE * 2, 640 - heigth_texture.get_height())
+		new_sprite.position = Vector2(i * Main.TEXTURE_SIZE * 2, 640 - height_texture.get_height())
 		new_sprite.hframes = 21
 		new_sprite.frame = 0
 		
-		heigth_sprites.append(new_sprite)
+		height_sprites.append(new_sprite)
 		add_child(new_sprite)
 
 func set_terrain_width_2(width : int) -> void:
@@ -29,7 +29,7 @@ func set_terrain_width_2(width : int) -> void:
 		var new_sprite : Sprite2D = Sprite2D.new()
 		new_sprite.texture = tile_texture
 		new_sprite.centered = false
-		new_sprite.scale = Vector2(1,0)
 		new_sprite.position = Vector2(i * Main.TEXTURE_SIZE, 636)
-		heigth_sprites.append(new_sprite)
+		new_sprite.scale = Vector2(1,0)
+		height_sprites.append(new_sprite)
 		add_child(new_sprite)
--- main.gd
+++ main.gd
@@ -5,23 +5,23 @@ class_name Main
 @onready var terrain_manager: Node2D = %"Terrain manager"
 @onready var label: Label = %Label
 
-var terrain_heigth : Array[int] = []
+var terrain_height : Array[int] = []
 
 const TERRAIN_MAX_WIDTH : int = 300
-const TERRAIN_MAX_HEIGTH : int = 40
+const TERRAIN_MAX_height : int = 40
 const TEXTURE_SIZE : int = 4
 
 func _ready() -> void:
 	Signals.add_value_to_terrain.connect(add_value_to_terrain_by_index)
 	
 	for i : int in range(TERRAIN_MAX_WIDTH):
-		terrain_heigth.append(0)
+		terrain_height.append(0)
 	
 func _process(delta: float) -> void:
 	label.text = "FPS: " + str(Engine.get_frames_per_second())
 
 
 func add_value_to_terrain_by_index(index : int) -> void:
-	terrain_heigth[index] = min(terrain_heigth[index] + 1, TERRAIN_MAX_HEIGTH)
-	terrain_manager.update_terrain(index, terrain_heigth[index])
+	terrain_height[index] = min(terrain_height[index] + 1, TERRAIN_MAX_height)
+	terrain_manager.update_terrain(index, terrain_height[index])
 
